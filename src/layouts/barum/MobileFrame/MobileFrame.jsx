29d41f9ea47a992a83c19cd44ebe92092f53fd55
iyeonggyu0 <iyeonggyu0@gmail.com>
2026-09-02 18:28:38 +0900

    [feature-23]  바름 추가 완료

--- src/layouts/barum/MobileFrame/MobileFrame.jsx
+++ src/layouts/barum/MobileFrame/MobileFrame.jsx
@@ -1,10 +1,13 @@
 /** @jsxImportSource @emotion/react */
+import { useDeviceMode } from "@/hooks/useDeviceMode";
 import { background, mobileContainer } from "./MobileFrame.style";
 
 const MobileFrame = ({ children }) => {
+  const { isMobile } = useDeviceMode();
+
   return (
-    <div css={background}>
-      <main css={mobileContainer}>{children}</main>
+    <div css={!isMobile ? background : {}}>
+      <main css={mobileContainer(isMobile)}>{children}</main>
     </div>
   );
 };
--- src/layouts/barum/MobileFrame/MobileFrame.style.js
+++ src/layouts/barum/MobileFrame/MobileFrame.style.js
@@ -11,18 +11,21 @@ export const background = css({
   position: "relative",
 });
 
-export const mobileContainer = css({
-  width: "390px",
-  height: "844px",
-  backgroundColor: theme.barum.colors.bg,
-  boxShadow: "0 24px 60px rgba(53, 60, 55, .01)",
-  position: "relative",
-  overflowX: "hidden",
-  overflowY: "auto",
-  borderRadius: "44px",
+export const mobileContainer = (isMobile) =>
+  css({
+    width: "100%",
+    maxWidth: isMobile ? "1199px" : "390px",
+    height: isMobile ? "100%" : "844px",
+    maxHeight: "100vh",
+    backgroundColor: theme.barum.colors.bg,
+    boxShadow: isMobile ? "0 24px 60px rgba(53, 60, 55, .01)" : "",
+    position: "relative",
+    overflowX: "hidden",
+    overflowY: "auto",
+    borderRadius: isMobile ? "" : "44px",
 
-  /* 스크롤바 숨김 처리 */
-  "&::-webkit-scrollbar": {
-    display: "none",
-  },
-});
+    /* 스크롤바 숨김 처리 */
+    "&::-webkit-scrollbar": {
+      display: "none",
+    },
+  });
